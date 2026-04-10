866880882d45cf7d993bd480c964a29527a63853
Giovana <giovana.dvalentini@gmail.com>
2026-04-10 09:36:32 -0300

    python cp1 esp32

---
+++ main.py
@@ -0,0 +1,27 @@
+# instalar no terminal: pip install paho-mqtt==1.5.0
+import paho.mqtt.client as mqtt
+
+def ao_conectar(client, userdata, flags, rc):
+    print("Nos conectamos com o seguinte código de resultado {}".format((rc)))
+    client.subscribe("giovana")
+
+'''
+<= 30 → perto → cheio
+<= 100 → médio → consumindo
+> 100 → longe → vazio
+'''
+def ao_receber(client, userdata, msg):
+    mensagem = msg.payload.decode()
+    print("Mensagem recebida:", mensagem)
+        
+cliente = mqtt.Client()
+
+cliente.on_connect = ao_conectar
+cliente.on_message = ao_receber
+cliente.connect("broker.hivemq.com", 1883, 60)
+cliente.loop_forever()
+
+cliente.loop_start()
+while True:
+    cliente.publish("giovana", input("Envie uma mensagem")) # topico pra enviar
+cliente.loop_finish()
