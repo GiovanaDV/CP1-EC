761d76cf77b11d409ee01f3c54d9be0446b7c7be
Giovana <giovana.dvalentini@gmail.com>
2026-04-10 09:45:35 -0300

    codigo

--- main.py
+++ main.py
@@ -2,21 +2,22 @@
 import paho.mqtt.client as mqtt
 
 def ao_conectar(client, userdata, flags, rc):
-    print("Nos conectamos com o seguinte código de resultado {}".format((rc)))
+    print("Conectado com código:", rc)
     client.subscribe("giovana")
 
 def ao_receber(client, userdata, msg):
     mensagem = msg.payload.decode()
-    print("Mensagem recebida:", mensagem)
-        
-cliente = mqtt.Client()
+    if mensagem == "silo abastecido":
+        print("✅ Silo abastecido")
+    elif mensagem == "silo em consumo":
+        print("⚠️  Silo em consumo")
+    elif mensagem == "silo vazio":
+        print("🚨 Silo vazio")
+    else:
+        print("Mensagem recebida:", mensagem)
 
+cliente = mqtt.Client()
 cliente.on_connect = ao_conectar
 cliente.on_message = ao_receber
 cliente.connect("broker.hivemq.com", 1883, 60)
-cliente.loop_forever()
-
-cliente.loop_start()
-while True:
-    cliente.publish("giovana", input("Envie uma mensagem")) # topico pra enviar
-cliente.loop_finish()
+cliente.loop_forever() 
